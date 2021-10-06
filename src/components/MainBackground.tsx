0910b749b7b0d03bd394c0bb537ada3069aa106d
JIYEON_OnTime <54522206+Ji-Yeon0107@users.noreply.github.com>
2021-10-06 15:53:13 +0900

    Add files via upload

--- src/components/MainBackground.tsx
+++ src/components/MainBackground.tsx
@@ -6,11 +6,11 @@ export const MainBackground = ()=>{
     return(
         <React.Fragment>
             <div>
-                <img className="bg" src="https://ji-yeon0107.github.io/learning/images/main0.jpg" alt="background"/>
+                <img className="bg" src="./images/main0.jpg" alt="background"/>
             </div>
             <div className="bg-box"></div>
             <div>
-                <img className="bg-box-init" src="https://ji-yeon0107.github.io/learning/main0.jpg" alt="background"/>
+                <img className="bg-box-init" src="./images/main0.jpg" alt="background"/>
             </div>
         </React.Fragment>
     )
